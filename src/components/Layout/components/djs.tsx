07f6e254190e03122c2b6c3c29c5bc9a015f613e
bndgnz <61714487+bndgnz@users.noreply.github.com>
2023-01-19 12:31:01 +1300

    css tweaks

--- src/components/Layout/components/djs.tsx
+++ src/components/Layout/components/djs.tsx
@@ -33,24 +33,21 @@ function Sponsors(props: any) {
   if (error) {
     return <div></div>;
   }
-  console.log(data);
 
-  const listOfItems = data.staffList.staffCollection.items.map(
-    (dj, idx) => {
-      return (
-        <div className="col-lg-3   col-xs-12" key={idx}>
-          <div className=" sponsor-card ">
-            <img src={dj.photo.url} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{dj.title}</h5>
+  const listOfItems = data.staffList.staffCollection.items.map((dj, idx) => {
+    return (
+      <div className="col-lg-3   col-xs-12" key={idx}>
+        <div className=" sponsor-card ">
+          <img src={dj.photo.url} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{dj.title}</h5>
 
-              {dj.shortBio}
-            </div>
+            {dj.shortBio}
           </div>
         </div>
-      );
-    }
-  );
+      </div>
+    );
+  });
   return (
     <section className="about-area ptb-100">
       <div className="container">
