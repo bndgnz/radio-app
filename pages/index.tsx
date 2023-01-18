3b77458e9fc079d5594cfca47d138cd1913ba9b2
bndgnz <61714487+bndgnz@users.noreply.github.com>
2023-01-19 08:30:11 +1300

    tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,24 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import { ILandingPageFields} from "../src/@types/contentful";
+import { ILandingPageFields } from "../src/@types/contentful";
 import styles from "../styles/Home.module.css";
 import ContentService from "@/src/utils/content-service";
- 
-import Layout from "@/src/components/Layout";
 
+import Layout from "@/src/components/Layout";
 
 interface Props {
   articles: ILandingPageFields[];
 }
 
-const Home: NextPage<Props> = ({ articles }) => (
-
-
-  
-
-<Layout props={articles}  />
- 
-
- 
-);
+const Home: NextPage<Props> = ({ articles }) => <Layout props={articles} />;
 
 export default Home;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const articles = (
-    await ContentService.instance.getEntriesByType<ILandingPageFields>("landingPage")
+    await ContentService.instance.getEntriesByType<ILandingPageFields>(
+      "landingPage"
+    )
   ).map((entry) => entry.fields);
 
   return {
--- src/components/Layout/components/sponsors.tsx
+++ src/components/Layout/components/sponsors.tsx
@@ -5,116 +5,59 @@ import { useRouter } from "next/router";
 import absoluteUrl from "next-absolute-url";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function Sponsors(props: any) {
-
- 
-
-
-    const id = props.id;
-    const PLAYLIST = gql`
-      query GetSponsors($id: String!) {
-        sponsorsList(id: $id) {
+  const id = props.id;
+  const PLAYLIST = gql`
+    query GetSponsors($id: String!) {
+      sponsorsList(id: $id) {
+        title
+        sponsorsCollection {
+          items {
             title
-            sponsorsCollection {
-              items {
-                title
-                image {url}
-                introduction
-              }
+            image {
+              url
             }
+            introduction
           }
+        }
       }
-      
-    `;
-
-    const { data, loading, error } = useQuery(PLAYLIST, {
-      variables: { id },
-    });
-    if (loading) {
-      return <div></div>;
     }
-    if (error) {
-      return <div></div>;
-    }
-console.log(data)
+  `;
 
- 
+  const { data, loading, error } = useQuery(PLAYLIST, {
+    variables: { id },
+  });
+  if (loading) {
+    return <div></div>;
+  }
+  if (error) {
+    return <div></div>;
+  }
+  console.log(data);
 
- 
-
-  
-    const listOfItems = data.sponsorsList.sponsorsCollection.items.map((sponsor, idx) => {
+  const listOfItems = data.sponsorsList.sponsorsCollection.items.map(
+    (sponsor, idx) => {
       return (
+        <div className="col-lg-3   col-xs-12" key={idx}>
+          <div className=" sponsor-card ">
+            <img src={sponsor.image.url} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{sponsor.title}</h5>
 
-
-<div className="col-lg-3   col-xs-12" key={idx}>
-        <div className=" sponsor-card ">
-    <img src={sponsor.image.url} className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">{sponsor.title}</h5>
-   
-{sponsor.introduction}
-
-
-
-
-    </div>
-  </div>
-
-  </div>
- 
-
- 
-      );
-    });
-    return (
-
-
-<section className="about-area ptb-100">
-      <div className="container">
-        
-          <div className="card-deck  ">
-      
-
-
-       {listOfItems}
-      </div>
+              {sponsor.introduction}
+            </div>
           </div>
-
-       
-     
+        </div>
+      );
+    }
+  );
+  return (
+    <section className="about-area ptb-100">
+      <div className="container">
+        <div className="card-deck  ">{listOfItems}</div>
+      </div>
     </section>
-
-
-
-
-
-
-
-
-
-
-     
-    );
-  
-
-   
+  );
 }
 
 export default Sponsors;
