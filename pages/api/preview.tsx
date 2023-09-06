a633890f3384f0f41a2f1f5e989897937de1f469
bndgnz <61714487+bndgnz@users.noreply.github.com>
2023-09-06 13:23:57 +1200

    content preview

--- pages/api/preview.tsx
+++ pages/api/preview.tsx
@@ -1,16 +1,35 @@
-export default async function handler(
-  req: {
-    query: {
-      slug: string;
-    };
-  },
-  res
-): Promise<any> {
-  if (!req.query.slug) {
-    return res.status(401).json({ message: 'Invalid slug' });
-  }
-  const location = 'https://www.waihekeradio.org.nz/' + req.query.slug;
-  res.setPreviewData({});
-  res.writeHead(307, { Location: location });
-  res.end();
+import { getPreviewPostBySlug } from "@/src/utils/api";
+import config from "@/src/components/Layout/config"   
+
+// http://localhost:9009/api/preview?secret=testing&slug=core-concepts
+
+export default async function preview(req, res) {
+
+    const { secret, slug } = req.query;
+ 
+    if (secret !== config.previewSecret || !slug) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+
+    // Fetch the headless CMS to check if the provided `slug` exists
+    const post = await getPreviewPostBySlug(slug);
+
+    // If the slug doesn't exist prevent preview mode from being enabled
+    if (!post) {
+        return res.status(401).json({ message: "Invalid slug" });
+    }
+
+    // Enable Preview Mode by setting the cookies
+    res.setPreviewData({}); // more info on this here -> https://nextjs.org/docs/advanced-features/preview-mode
+
+    // Redirect to the path from the fetched post
+    // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
+    // res.writeHead(307, { Location: `/posts/${post.slug}` })
+    const url = `/${post.slug}`;
+    res.write(
+        `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
+    <script>window.location.href = '${url}'</script>
+    </head>`
+    );
+    res.end();
 }
---
+++ src/components/Layout/config.js
@@ -0,0 +1,9 @@
+// All environment variables will be sourced
+// Do NOT commit this file to source control
+module.exports = {
+space_id: 'muwn01agnrp5',
+delivery_token: '69d7oeasRdTU7_W9K2x9IS8bO2r53ln1vzZyFMTKLAA',
+preview_token: 'okWASikEbrrU-mJOUgE-Cu9f8E_BD7mLxUZbbhJKcYA',
+previewSecret: 'preview',
+environment: 'master'
+}
